47da7ae3089f3f78907dfa7a40755d5b0903346f
Ritik Singh <82249834+Ritik1104@users.noreply.github.com>
2024-04-04 08:49:23 +0530

    Create useMovieTrailer.js

---
+++ src/hooks/useMovieTrailer.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { API_options } from '../Utils/Constants'
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideos } from '../Utils/moviesSlice';
+
+
+const useMovieTrailer = (movieId) => {
+    const dispatch = useDispatch();
+    const trailerVideo = useSelector(store => store.movies.trailerVideo);
+
+
+
+    useEffect(() => {
+        const getMoviesVideos = async () => {
+            const data = await fetch("https://api.themoviedb.org/3/movie/" + movieId + "/videos?language=en-US", API_options);
+            const json = await data.json();
+
+            const filterData = json.results.filter((video) => video.type === "Trailer");
+            const trailer = filterData.length ? filterData[0] : json.results[0];
+            dispatch(addTrailerVideos(trailer));
+        }
+        if (!trailerVideo) getMoviesVideos();
+    }, [movieId, dispatch, trailerVideo]);
+}
+
+export default useMovieTrailer;
